8f96d9c137003397846cf91f598ecd4eb9f136ec
MayankSrivastav <mayanksri18@yahoo.in>
2017-03-01 00:28:20 +0530

    Initial commit

---
+++ test/immutable_spec.js
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+
+describe('immutablity', () => {
+    describe('a number', () => {
+        function increment(currentState) {
+            return currentState + 1;
+        }
+
+        if('is immutable', () => {
+            let state = 42;
+            let nextState = increment(state);
+            expect(nextState).to.equal(43);
+            expect(state).to.equal(42);
+        });
+    });
+});
