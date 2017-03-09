95477810f6c56580629e7e5daefa38fc7ed730ee
MayankSrivastav <mayanksri18@yahoo.in>
2017-03-09 20:47:02 +0530

    Added comments

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
+// Main server file to initiate server
+
 import Server from 'socket.io';
 
+// Export startServer function to be
+// used by other modules
 export default function startServer() {
     const io = new Server().attach(8090);
 }
 
+// Subscribe to the store to get the state
+// changes
 store.subscribe(
     () => io.emit('state', store.getState().toJS())
 );
