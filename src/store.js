188ca8728a829c7b031e50f8c8ec5ba61cbf9a1b
MayankSrivastav <mayanksri18@yahoo.in>
2017-03-09 20:53:06 +0530

    Updated comments

--- src/store.js
+++ src/store.js
@@ -3,6 +3,8 @@
 import {createStore} from 'redux';
 import reducer from './reducer';
 
+// Create the store passing the 
+// reducer as the argument
 export default function makeStore() {
     return createStore(reducer);
 }
