3854a3fc147e4d83fd567e9e4556c2af33decc0b
MayankSrivastav <mayanksri18@yahoo.in>
2017-03-09 20:51:43 +0530

    Added comments

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+// Index file to start the operations
+
 import makeStore from './src/store';
 import startServer from './src/server';
 
+// Make the store
 export const store = makeStore();
+
+// Start the server with store
 startServer(store);
 
+// Dispatch SET_ENTRIES action
 store.dispatch({
     type: 'SET_ENTRIES',
     entries: require('../entries.json')
