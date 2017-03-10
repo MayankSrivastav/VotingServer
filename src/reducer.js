e0f2438dad7d0a5ffe035a337a97cc685875ec83
MayankSrivastav <mayanksri18@yahoo.in>
2017-03-10 16:24:58 +0530

    Updated comments

--- src/reducer.js
+++ src/reducer.js
@@ -5,7 +5,7 @@ export default function reducer(state = INITIAL_STATE, action) {
     switch(action.type) {
         case 'SET_ENTRIES': // Call SET_ENTRIES action
             return setEntries(state, action.entries);
-        case 'NEXT':
+        case 'NEXT': // Call NEXT action
             return next(state);
         case 'VOTE':
             return state.update('vote',
